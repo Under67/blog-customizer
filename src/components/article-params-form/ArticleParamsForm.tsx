4f90f1e527ea5613c6cbb9a53df5adeb59f3dec8
Evgeniy Kryizhanovskiy <under67@mail.ru>
2025-07-26 15:15:38 +0300

    ver 1.0

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,15 +2,113 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { SyntheticEvent, useState } from 'react';
+import clsx from 'clsx';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	onApply: (values: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ onApply }: Props) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [selectedFontSize, setSelectedFontSize] = useState<OptionType>(
+		fontSizeOptions[0]
+	);
+	const [selectedFontFamily, setSelectedFontFamily] = useState<OptionType>(
+		fontFamilyOptions[0]
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState<OptionType>(
+		fontColors[0]
+	);
+	const [selectedBackgroundColors, setSelectedBackgroundColors] =
+		useState<OptionType>(backgroundColors[0]);
+	const [selectedContentWidthArr, setSelectedContentWidthArr] =
+		useState<OptionType>(contentWidthArr[0]);
+	function handleReset() {
+		setSelectedFontSize(fontSizeOptions[0]);
+		setSelectedFontFamily(fontFamilyOptions[0]);
+		setSelectedFontColor(fontColors[0]);
+		setSelectedBackgroundColors(backgroundColors[0]);
+		setSelectedContentWidthArr(contentWidthArr[0]);
+		onApply({
+			fontSizeOption: selectedFontSize,
+			fontFamilyOption: selectedFontFamily,
+			fontColor: selectedFontColor,
+			backgroundColor: selectedBackgroundColors,
+			contentWidth: selectedContentWidthArr,
+		});
+	}
+	function handleSubmit(event: SyntheticEvent<HTMLFormElement>) {
+		event.preventDefault();
+		onApply({
+			fontSizeOption: selectedFontSize,
+			fontFamilyOption: selectedFontFamily,
+			fontColor: selectedFontColor,
+			backgroundColor: selectedBackgroundColors,
+			contentWidth: selectedContentWidthArr,
+		});
+	}
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h2' weight={800} uppercase size={31}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={selectedFontFamily}
+						onChange={setSelectedFontFamily}
+					/>
+					<RadioGroup
+						name='font-size'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={selectedFontSize}
+						onChange={setSelectedFontSize}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={selectedFontColor}
+						onChange={setSelectedFontColor}
+					/>
+					<Separator></Separator>
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={selectedBackgroundColors}
+						onChange={setSelectedBackgroundColors}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={selectedContentWidthArr}
+						onChange={setSelectedContentWidthArr}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, ArticleStateType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +12,23 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+
 const App = () => {
+	const [articleStyles, setArticleStyles] = useState<ArticleStateType>(defaultArticleState);
+
+	const style={
+  '--font-family': articleStyles.fontFamilyOption.value,
+  '--font-size': articleStyles.fontSizeOption.value,
+  '--font-color': articleStyles.fontColor.value,
+  '--container-width': articleStyles.contentWidth.value,
+  '--bg-color': articleStyles.backgroundColor.value,
+} as CSSProperties
 	return (
 		<main
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			style={style}
+			>
+			<ArticleParamsForm onApply={setArticleStyles}/>
 			<Article />
 		</main>
 	);
